658d418064ed65255967bec3c0c08bc30dca433a
Ande Caleb <andaeiii@aol.com>
2021-03-14 13:38:32 +0100

    Restructuring and moving functionality to main App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Counters from './components/counters';
 import NavBar from './components/navbar'
 
-function App() {
-  return (
+class App extends Component {
 
-    <>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  }
 
-    </>
 
-  );
+  handleIncrement = (counter) => {
+    //clone the array..
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);    //get the position of that counter... 
+    counters[index] = { ...counter }; //copy the counter from the parameter sent... 
+    counters[index].value++;
+    this.setState({ counters })
+  }
+
+
+  //to delete... 
+  handleDelete = (counterId) => {
+    //console.log('Event Handler Called...', counterId)
+
+    const remainderCounters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: remainderCounters });
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters: counters });
+  }
+
+  render() {
+
+    return (
+      <>
+        <NavBar />
+        <main className="container">
+
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+
+        </main>
+
+      </>
+    );
+  }
+
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,59 +2,8 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {
-        counters: [
-            { id: 1, value: 4 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
-    }
 
 
-    handleIncrement = (counter) => {
-        //clone the array..
-        const counters = [...this.state.counters];
-        const index = counters.indexOf(counter);    //get the position of that counter... 
-        counters[index] = { ...counter }; //copy the counter from the parameter sent... 
-        counters[index].value++;
-        this.setState({ counters })
-    }
-
-
-    //to delete... 
-    handleDelete = (counterId) => {
-        //console.log('Event Handler Called...', counterId)
-
-        const remainderCounters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters: remainderCounters });
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        });
-
-        this.setState({ counters: counters });
-    }
-
-    //when an object is declared like this ---  ({counter}) = ({counter: counter}) //key and variable name are the same... 
-
-
-    /*
- 
-         map(counter => 
-             <Counter
-                 key={counter.id}
-                 selectedId={true}
-                 onDelete={this.handleDelete}
-                 id={counter.id}
-                 value={counter.value}
-               />
-           )
- 
-    */
 
     render() {
         return (
@@ -62,29 +11,27 @@ class Counters extends Component {
 
                 <button
                     className="btn.btn-primary btn-sm m-3 p-8"
-                    onClick={this.handleReset}>
+                    onClick={this.props.onReset}>
                     reset
                 </button>
 
 
                 {
-                    this.state.counters.map(c =>
+                    this.props.counters.map(c =>
 
                         <Counter
                             key={c.id}
                             selected={true}
-                            onIncrement={this.handleIncrement}
-                            onDelete={this.handleDelete}
+
+                            onIncrement={this.props.onIncrement}
+                            onDelete={this.props.onDelete}
+
                             id={c.id}
                             value={c.value}
                             counter={c}
 
                         />
 
-
-
-                        /* { <h4>Counter #{c.id} </h4> }
-                    </Counter>*/
                     )
                 }
 
